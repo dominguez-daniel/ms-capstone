4188fbde029ef7dd58a4b1a770b5155d479dafcb
Daniel Dominguez <daniel@Daniels-MBP.attlocal.net>
2023-04-25 16:11:47 -0500

    troubleshooting

--- input_sort.sh
+++ input_sort.sh
@@ -2,4 +2,4 @@
 PORT=8080
 curl -d '{ "input": [10, 2, 9, 3, 8, 4, 7, 6, 5, 1] }' \
      -H "Content-Type: application/json" \
-     -X POST https://a084a606b6f9c42d09a43af6dc3cc003-1712573430.us-east-1.elb.amazonaws.com:8080/:$PORT/sort
+     -X POST http://127.0.0.1:$PORT/sort
